834e02747b40f3279d50099e631e35dea478f47f
Kobayasi <8066248+myjiee@user.noreply.gitee.com>
2022-09-22 22:17:32 +0800

    兼容批量执行用例

--- apps/run_case/router.py
+++ apps/run_case/router.py
@@ -11,6 +11,7 @@
 from requests.exceptions import ConnectionError
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
+from typing import List
 from depends import get_db
 from setting import ALLURE_PATH
 from apps import response_code
@@ -23,56 +24,55 @@ from tools.load_allure import load_allure_report
 run_case = APIRouter()
 
 
-@run_case.post('/{case_id}', name='按用例执行')
-async def run_case_name(request: Request, case_id: int, db: Session = Depends(get_db)):
-    case_info = await case_crud.get_case_name(db=db, case_id=case_id)
-    if case_info:
-        # 拿到测试数据
-        case_data = await case_crud.get_case_data(db=db, case_id=case_info[0].id)
-        # 拿到模板数据
-        temp_data = await temp_crud.get_template_data(db=db, temp_id=case_info[0].temp_id)
-        # 拿到项目名称、模板名称
-        temp_info = await temp_crud.get_temp_name(db=db, temp_id=case_info[0].temp_id)
-        # 处理数据，执行用例
-        try:
-            case, run_order = await RunCase().fo_service(
-                db=db,
-                case_id=case_id,
-                temp_data=temp_data,
-                case_data=case_data,
-                temp_pro=temp_info[0].project_name,
-                temp_name=temp_info[0].temp_name
+@run_case.post('/', name='按用例执行')
+async def run_case_name(request: Request, case_id: int = None, case_ids: List[int] = None,
+                        db: Session = Depends(get_db)):
+    if not case_id and not case_ids:
+        return await response_code.resp_400()
+
+    if case_id:
+        case_ids = [case_id]
+
+    report = []
+    for case_id in case_ids:
+        case_info = await case_crud.get_case_name(db=db, case_id=case_id)
+        if case_info:
+            # 拿到测试数据
+            case_data = await case_crud.get_case_data(db=db, case_id=case_info[0].id)
+            # 拿到模板数据
+            temp_data = await temp_crud.get_template_data(db=db, temp_id=case_info[0].temp_id)
+            # 拿到项目名称、模板名称
+            temp_info = await temp_crud.get_temp_name(db=db, temp_id=case_info[0].temp_id)
+            # 处理数据，执行用例
+            try:
+                case, run_order = await RunCase().fo_service(
+                    db=db,
+                    case_id=case_id,
+                    temp_data=temp_data,
+                    case_data=case_data,
+                    temp_pro=temp_info[0].project_name,
+                    temp_name=temp_info[0].temp_name
+                )
+            # except (ServerDisconnectedError, ServerConnectionError) as e:
+            except (ConnectionError,) as e:
+                return await response_code.resp_400(message=f'网络访问失败: {str(e)}')
+
+            except IndexError as e:
+                return await response_code.resp_400(message=f': {str(e)}')
+
+            # 校验结果，生成报告
+            allure_dir = ALLURE_PATH
+            await run(
+                test_path='./apps/run_case/test_case/test_service.py',
+                allure_dir=allure_dir,
+                report_url=request.base_url,
+                case_name=case,
+                case_id=case_id
             )
-        # except (ServerDisconnectedError, ServerConnectionError) as e:
-        except (ConnectionError,) as e:
-            return await response_code.resp_400(message=f'网络访问失败: {str(e)}')
+            load_allure_report(allure_dir=allure_dir, case_id=case_id, run_order=run_order)
 
-        except IndexError as e:
-            return await response_code.resp_400(message=f': {str(e)}')
+            report.append(f'{request.base_url}allure/{case_id}/{run_order}')
+        else:
+            report.append(f'用例{case_id}不存在')
 
-        # 校验结果，生成报告
-        allure_dir = ALLURE_PATH
-        await run(
-            test_path='./apps/run_case/test_case/test_service.py',
-            allure_dir=allure_dir,
-            report_url=request.base_url,
-            case_name=case,
-            case_id=case_id
-        )
-        load_allure_report(allure_dir=allure_dir, case_id=case_id, run_order=run_order)
-
-        return await response_code.resp_200(
-            data={'allure_report': f'{request.base_url}allure/{case_id}/{run_order}'}
-        )
-    else:
-        return await response_code.resp_404()
-
-
-@run_case.post('/ids', name='按用例ID执行')
-async def run_case_ids(ids: list, db: Session = Depends(get_db)):
-    pass
-
-
-@run_case.post('/tempName', name='按模块执行')
-async def run_temp_name(temp_name: str, db: Session = Depends(get_db)):
-    pass
+    return await response_code.resp_200(data={'allure_report': report})
